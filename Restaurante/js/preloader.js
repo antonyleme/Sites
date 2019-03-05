7842e353dc0b794946fe03c29ba9deaa07fc6343
Antony Leme <antony040402@gmail.com>
2019-03-05 17:52:09 -0300

    preloader

--- Restaurante/js/preloader.js
+++ Restaurante/js/preloader.js
@@ -1,8 +1,7 @@
 var $content = $('#content');
 var $preloader = $('#preloader');
-$content.hide();
 
 window.onload = function(){
     $preloader.hide();
-    $content.show();
+    $content.removeClass("d-none");
 }
