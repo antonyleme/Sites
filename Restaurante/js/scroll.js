da15a615b1dded07ad38415d7a771d5deb3f5cf6
Antony Leme <antonycefet@gmail.com>
2019-07-25 18:26:51 -0300

    layout

--- Restaurante/js/scroll.js
+++ Restaurante/js/scroll.js
@@ -1,4 +1,6 @@
-var $items = $('.nav-link');
+var $items = $('.link-scroll');
+
+console.log('teste')
 
 $items.bind('click', function(){
     $items.each(function(){
@@ -13,9 +15,24 @@ $(document).ready(function(){
         var linkhref = $(this).attr('href');
 
         $('html, body').animate({
-            scrollTop: $(linkhref).offset().top - 100
+            scrollTop: $(linkhref).offset().top - 60
         });
 
         e.preventDefault();
     });
+});
+
+$(document).scroll(function(){
+    $logodesk = $('.logo-desk');
+    $logoscroll = $('.logo-scroll');
+    $logobg =  $('.logo-desk-bg');
+	if($(document).scrollTop() == 0){
+        $logodesk.show();
+        $logoscroll.hide();
+        $logobg.show();
+	}else{
+        $logodesk.hide();
+        $logoscroll.show();
+        $logobg.hide();
+	}
 });
